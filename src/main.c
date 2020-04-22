f93f499571cd174fe3964d035aab573d88e5a5ce
Baptiste Zammit <baptiste.zammit@epita.fr>
2020-04-22 22:16:56 +0200

    [Words] - Random word

--- src/main.c
+++ src/main.c
@@ -3,5 +3,7 @@
 int main(void){
     int numb = randomizer();
     printf("number : %d\n", numb);
+    char *chosen_word = word_from_file(numb);
+    printf("word : %s", chosen_word);
     return 0;
 }
--- src/random.h
+++ src/random.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 
 int randomizer(void);
+char *word_from_file(int numb);
