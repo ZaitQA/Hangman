6d7fc921dbe925714cf92e0b6cdb11aabb0e109f
Baptiste Zammit <baptiste.zammit@epita.fr>
2020-04-22 22:16:56 +0200

    [Words] - Random word

--- src/random.c
+++ src/random.c
@@ -3,3 +3,23 @@
 int randomizer(void){
     return (rand() % (370098));
 }
+
+char *word_from_file(int numb){
+    FILE *file = fopen("words_alpha.txt", "r");
+    if (!file){
+        err(1, "The database file can not be opened");
+    }
+    int count = 0;
+    char line[50];
+    char *chosen_word = calloc(30, sizeof(char));
+    while (fgets(line, sizeof line, file)){
+        if (numb == count){
+            chosen_word = line;
+            fclose(file);
+            return chosen_word;
+        }
+        count++;
+    }
+    fclose(file);
+    err(1, "Word has not be found");
+}
