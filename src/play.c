13e3b77e2dba6d0f59aa363a0c5c0d7622a9fcfe
Hugo Souillac <hugo.souillac@epita.fr>
2020-04-22 22:39:39 +0200

    Do you want to play : question for user before playing

---
+++ src/play.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//pre-game : do you want to play ?
+int do_you_want()
+{
+    char *answer = calloc(25, sizeof(char));
+    printf("Do you want to play Hangman game?(yes/no)\n");
+    scanf("%s", answer);
+    if (strcmp(answer, "yes") == 1)
+    {
+        printf("Ok, bye then!\n");
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+
+int main()
+{
+    do_you_want();
+    return 0;
+}
