77d5fe99b7477e7ccfed7c3adf765b6f80226977
Baptiste Zammit <baptiste.zammit@epita.fr>
2020-04-23 10:28:21 +0200

    [FIX] - Fix the code

--- src/play.c
+++ src/play.c
@@ -3,18 +3,14 @@
 #include <string.h>
 
 //pre-game : do you want to play ?
-int do_you_want()
-{
-    char *answer = calloc(25, sizeof(char));
+int do_you_want(){
+    char *answer = calloc(4, sizeof(char));
     printf("Do you want to play Hangman game?(yes/no)\n");
     scanf("%s", answer);
-    if (strcmp(answer, "yes") == 1)
-    {
+    if (strcmp(answer, "yes") == 1){
         printf("Ok, bye then!\n");
         return 1;
-    }
-    else
-    {
+    }else{
         return 0;
     }
 }
