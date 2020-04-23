9b635f4af0da9ab7425ce171296dcd0fe4502259
Baptiste Zammit <baptiste.zammit@epita.fr>
2020-04-23 17:13:15 +0200

    [FIX] - Fix the code

--- src/play.c
+++ src/play.c
@@ -9,30 +9,23 @@ int letter_tested[26] = {0};      //all letters the player will have tested
 short tries = 0;
 
 //do you want to play ?
-int do_you_want()
-{
+int do_you_want(){
     char *answer = calloc(25, sizeof(char));
     question_playing();
     scanf("%s", answer);
-    if (strcmp(answer, "yes") == 1)
-    {
+    if (strcmp(answer, "yes") == 1){
         no_play();
         return 1;
-    }
-    else
-    {
+    }else{
         yes_play();
         return 0;
     }
 }
 
 //test if the given char is in the hidden word
-int is_in(char *chosen_word, long len_w, char test)
-{
-    for (int i = 0; i < len_w; i++)
-    {
-        if (chosen_word[i] == test)
-        {
+int is_in(char *chosen_word, long len_w, char test){
+    for (int i = 0; i < len_w; i++){
+        if (chosen_word[i] == test){
             return 0;
         }
     }
@@ -40,17 +33,14 @@ int is_in(char *chosen_word, long len_w, char test)
 }
 
 //test if the word gave by the player is the hidden word
-int is_it(char *chosen_word, char *test)
-{
+int is_it(char *chosen_word, char *test){
     return strcmp(chosen_word, test);
 }
 
 //calcul real size of the input
-int size_of(char *input)
-{
+int size_of(char *input){
     int i = 0;
-    while (input[i])
-    {
+    while (input[i]){
         i++;
     }
     return i;
@@ -61,55 +51,38 @@ void start_the_game(char *chosen_word)
 {
     printf("%s\n", chosen_word);
     char proposition[26];
-
     suggest();
     scanf("%s", proposition);
     int len_p = size_of(proposition);
     int len_input = size_of(chosen_word);
     printf("%d\n", len_input);
     printf("%s\n", chosen_word);
-
-    if (len_p == 1)
-    {
+    if (len_p == 1){
         int pos = proposition[0] - 97;
-        if (letter_tested[pos] == 1)
-        {
+        if (letter_tested[pos] == 1){
             already_gave();
             start_the_game(chosen_word);
         }
-        else
-        {
-            if (is_in(chosen_word, len_input, proposition[0]) == 0)
-            {
+        else{
+            if (is_in(chosen_word, len_input, proposition[0]) == 0){
                 right_letter();
                 start_the_game(chosen_word);
-            }
-            else
-            {
+            }else{
                 wrong_letter();
                 tries++;
                 start_the_game(chosen_word);
             }
             letter_tested[pos] = 1;
         }
-    }
-
-    else if (len_p == len_input)
-    {
-        if (is_it(chosen_word, proposition) == 0)
-        {
+    }else if (len_p == len_input){
+        if (is_it(chosen_word, proposition) == 0){
             find_word();
-        }
-        else
-        {
+        }else{
             wrong_word();
             tries += 3;
             start_the_game(chosen_word);
-        } 
-    }
-
-    else
-    {
+        }
+    }else{
         false_entry();
         tries += 3;
         start_the_game(chosen_word);
